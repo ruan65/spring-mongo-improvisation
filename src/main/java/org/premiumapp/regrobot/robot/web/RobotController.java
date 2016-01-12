3833cff2cee48a456cdcab83dcc7063f361ff144
ruan65 <ruan65@gmail.com>
2016-01-12 20:24:35 +0300

    final commit

--- src/main/java/org/premiumapp/regrobot/robot/web/RobotController.java
+++ src/main/java/org/premiumapp/regrobot/robot/web/RobotController.java
@@ -1,8 +1,10 @@
 package org.premiumapp.regrobot.robot.web;
 
+import org.apache.log4j.Logger;
 import org.premiumapp.regrobot.robot.Robot;
 import org.premiumapp.regrobot.robot.RobotService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,14 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RobotController {
 
-    @Autowired
-    RobotService robotService;
+    @Autowired RobotService robotService;
+
+    @Autowired CounterService counterService;
+
+    private Logger log = Logger.getLogger(RobotController.class);
+
+
 
     @RequestMapping(value = "/register/robot", method = RequestMethod.POST)
     public RegistrationResponse registerRobot(@RequestBody RegistrationRequest request) {
         Robot robot = new Robot();
         robot.setName(request.getName());
         robot.setAddress(request.getAddress());
+
+        counterService.increment("registration.register");
+
+        log.info(String.format("Registering robot %s at address %s", robot.getName(), robot.getAddress()));
 
         return new RegistrationResponse(robotService.registerRobot(robot));
     }
