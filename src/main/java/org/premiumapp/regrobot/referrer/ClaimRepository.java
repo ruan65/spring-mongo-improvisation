a3898b5faa9d303aebeb069f04883985d181a8ec
ruan65 <ruan65@gmail.com>
2016-02-02 11:44:31 +0300

    mongo requests via rest are working

--- src/main/java/org/premiumapp/regrobot/referrer/ClaimRepository.java
+++ src/main/java/org/premiumapp/regrobot/referrer/ClaimRepository.java
@@ -1,5 +1,13 @@
 package org.premiumapp.regrobot.referrer;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface ClaimRepository extends MongoRepository<Claim, String>{}
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "claim", path = "claim")
+public interface ClaimRepository extends MongoRepository<Claim, String>{
+
+    List<Claim> findByForename(@Param("forename") String name);
+}
